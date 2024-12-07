5be33fe75123eb147c8abd80c8300d4f005c1a3b
Ebrahim Shaban <126909248+EbrahimShabann@users.noreply.github.com>
2024-12-07 22:55:19 +0200

    Add files via upload

---
+++ Buky.Models/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		[MaxLength(30)]
+		public string Name { get; set; }
+        public string? Brand { get; set; }
+
+        public string Description { get; set; }
+
+        [Display(Name = "List Price")]
+		[Range(1,1000)]
+        public double ListPrice { get; set; }
+
+		[Display(Name = "Price of 1:50 piece")]
+		[Range(1,1000)]
+        public double Price1 { get; set; }
+		
+		[Display(Name = "Price of +50 piece")]
+		[Range(1,1000)]
+        public double Price50 { get; set; }
+		
+		[Display(Name = "Price of +100 piece")]
+		[Range(1,1000)]
+        public double Price100 { get; set; }
+		
+		
+		public int CategoryId { get; set; }
+		[ForeignKey(nameof(CategoryId))]
+		[ValidateNever]
+        public Category Category { get; set; }
+		[ValidateNever]
+        public List<ProductImage> ProductImages { get; set; }
+    }
+}
---
+++ Buky.Models/ViewModels/UserVM.cs
@@ -0,0 +1,19 @@
+﻿using Bulky.Models.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models.ViewModels
+{
+    public class UserVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> RolesList { get; set; } 
+        public IEnumerable<SelectListItem> CompaniesList { get; set; } 
+    }
+}
